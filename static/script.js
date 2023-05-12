a6e8f1e8e41af1cda89a76bf99fec5016f9a6fcc
Wasilewski6860 <91448408+Wasilewski6860@users.noreply.github.com>
2023-05-13 00:44:29 +0300

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,13 +1,1 @@
-function updateVal(){
-    sum = document.getElementById("sum").value;
-    pr = document.getElementById("pr").value;
-    term = document.getElementById("term").value;
 
-    sumVal = document.getElementById("sumVal");
-    prVal = document.getElementById("prVal");
-    termVal = document.getElementById("termVal");
-
-    sumVal.textContent = sum+"млн";
-    prVal.textContent = pr+"%";
-    termVal.textContent = term+"мес";
-}
